f6fffc48623e9cfd3aee62d7b8965c44f3cbc99e
Binfoo <julie0005@ajou.ac.kr>
2024-04-06 01:59:40 +0900

    init

---
+++ src/main/java/com/aws/demoserver/HealthController.java
@@ -0,0 +1,13 @@
+package com.aws.demoserver;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String checkHealth() {
+        return "UP";
+    }
+}
